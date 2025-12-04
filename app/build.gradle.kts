e5e3c30e9fc2a31b8a1111cc2225a092ab42a022
Misakaiscute <horchy.hunorpihe@gmail.com>
2025-12-04 10:49:24 +0100

    Added Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.dagger.hilt.android)
     ksp(libs.dagger.hilt.android.compiler)
+    implementation(libs.retrofit)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
