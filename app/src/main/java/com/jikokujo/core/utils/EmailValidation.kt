b318ce72b605c27ed0a656863fb1333b42ac77ec
Misakaiscute <horchy.hunorpihe@gmail.com>
2026-02-05 22:51:10 +0100

    Fixed incorrect email validation

--- app/src/main/java/com/jikokujo/core/utils/EmailValidation.kt
+++ app/src/main/java/com/jikokujo/core/utils/EmailValidation.kt
@@ -1,7 +1,7 @@
 package com.jikokujo.core.utils
 
 fun validateEmail(email: String): Boolean {
-    Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$")
+    Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+'\'.[a-zA-Z]{2,}$")
         .matchEntire(email)
         ?.let {
             return true
